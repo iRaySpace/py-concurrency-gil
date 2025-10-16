b1f4316774e58a060f59423b7049d4aa42691a83
irayspace <irayspacii@gmail.com>
2025-10-16 18:13:46 +0800

    feat: use logger for BigDto

--- backend/big_dto/big_dto.py
+++ backend/big_dto/big_dto.py
@@ -1,9 +1,15 @@
+from logger import get_logger
+
+logger = get_logger()
+
+
 # NOTE: I calculated it for 2GB. Because I tried around 10MB and it crashed my WSL, lol. 
 SIZE = 256 * 1024
 
 
 class BigDto:
-    def __init__(self):
+    def __init__(self, id):
+        self.id = id
         self.obj = bytearray(SIZE)
     def __del__(self):
-        print(f"BigDto destroyed")
+        logger.info(f"BigDto {self.id} destroyed")
