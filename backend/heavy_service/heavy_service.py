c7fa73fc207050ec925414be326a03e0388dc9a9
irayspace <irayspacii@gmail.com>
2025-10-16 18:17:29 +0800

    feat(heavy_service): log player

--- backend/heavy_service/heavy_service.py
+++ backend/heavy_service/heavy_service.py
@@ -1,3 +1,9 @@
+from big_dto.big_dto import BigDto
+from logger import get_logger
+
+logger = get_logger()
+
+
 # NOTE: Just a simple fibonacci to simulate CPU-bound tasks
 # This is implemented as top because Process Pool needs picklable
 _cache = {}
@@ -12,6 +18,7 @@ def _fibo(n: int):
     _cache[n] = result
     return result
 
-def run(n: int):
+def run(n: int, player: BigDto):
     result = _fibo(n) # First: Fibonacci
+    logger.info(f"Player {player.id} is finished playing.")
     return result
