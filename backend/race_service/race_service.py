72e8d2e233515c519c04566168d820d99ea5723e
irayspace <irayspacii@gmail.com>
2025-10-16 17:26:11 +0800

    feat(race_service): replicate by using temp and fix with lock

--- backend/race_service/race_service.py
+++ backend/race_service/race_service.py
@@ -7,15 +7,14 @@ _lock = threading.Lock()
 
 def increment():
     global _existing_players
-    time.sleep(0.05)
-    _existing_players = _existing_players + 1
+    temp = _existing_players
+    time.sleep(0.001)
+    _existing_players = temp + 1
 
 
 def increment_with_lock():
-    global _existing_players
-    time.sleep(0.05)
     with _lock:
-        _existing_players = _existing_players + 1
+        increment()
 
 
 def get_existing_players():
